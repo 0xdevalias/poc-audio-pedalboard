c1f5bbf0b7fe006029faec7a05d833a7197638b1
Glenn 'devalias' Grant <glenn@devalias.net>
2024-04-05 18:01:37 +1100

    commit local working state from when I last hacked on this (may or may not be runnable currently)

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # poc-audio-pedalboard
-Proof of Concept (PoC) code/notes exploring using the Pedalboard library for interacting with audio plugin VSTs/etc
+
+Proof of Concept (PoC) code/notes exploring using the [spotify/pedalboard](https://github.com/spotify/pedalboard) library for interacting with audio plugin VSTs/etc.
+
+## Prerequisites
+
+- Python 3.7+
+- pip
+- virtualenv
+
+## Installation
+
+First, clone the repository and navigate to the project directory:
+
+```bash
+git clone [repository-url]
+cd [repository-directory]
+```
+
+Create a virtual environment and activate it:
+
+```bash
+python -m venv venv
+source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+
+# Or:
+#   pyenv virtualenv 3.10.2 poc-audio-pedalboard
+#   pyenv local poc-audio-pedalboard
+```
+
+Install the required dependencies with:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Update the script with the correct path to your Vital VST3 or Component file. Then run the script within the virtual environment to load Vital and process MIDI through the synthesizer.
+
+```bash
+python synth_vst_loader.py
+```
+
+Make sure to check the Pedalboard documentation for more detailed usage and advanced features.
+
+## See Also
+
+### My Other Related Deepdive Gist's and Projects
+
+- [Generating Synth Patches with AI (0xdevalias' gist)](https://gist.github.com/0xdevalias/5a06349b376d01b2a76ad27a86b08c1b#generating-synth-patches-with-ai)
+- [Music APIs and DBs (0xdevalias' gist)](https://gist.github.com/0xdevalias/eba698730024674ecae7f43f4c650096#music-apis-and-dbs)
+- [AI Voice Cloning / Transfer (eg. RVCv2) (0xdevalias' gist)](https://gist.github.com/0xdevalias/359f4265adf03b0142e4d0543c156a3e#ai-voice-cloning--transfer-eg-rvcv2)
+- [Singing Voice Synthesizers (eg. Vocaloid, etc) (0xdevalias' gist)](https://gist.github.com/0xdevalias/0b64b25d72cbbc784042a9fdff713129#singing-voice-synthesizers-eg-vocaloid-etc)
+- [Audio Pitch Correction (eg. autotune, melodyne, etc) (0xdevalias' gist)](https://gist.github.com/0xdevalias/7f4a5c31758e04aea5c2f5520e53accb#audio-pitch-correction-eg-autotune-melodyne-etc)
+- [Compare/Diff Audio Files (0xdevalias' gist)](https://gist.github.com/0xdevalias/91ae33e0c9290e69b457ce5034956fb7#comparediff-audio-files)
---
+++ helpers.py
@@ -0,0 +1,113 @@
+from pprint import pprint, pformat
+
+from pedalboard import load_plugin, VST3Plugin, AudioUnitPlugin
+
+def pprint_with_indent(data, indent=4):
+    output = pformat(data)
+    indented_output = (' ' * indent) + ('\n' + ' ' * indent).join(output.splitlines())
+    print(indented_output)
+
+# Filter the locally installed audio plugins by the provided names
+#
+# Example usage:
+#   filter_installed_plugins_by_names(
+#     ['Vital', 'Serum']
+#    )
+def filter_installed_plugins_by_names(filters):
+    print("Showing installed VST/AU plugins, filtered by names:")
+
+    plugins_filter = {name.lower() for name in filters}
+    filtered_vst3_plugins = [
+        plugin_path for plugin_path in VST3Plugin.installed_plugins
+        if any(plugin_name in plugin_path.lower() for plugin_name in plugins_filter)
+    ]
+
+    filtered_au_plugins = [
+        plugin_path for plugin_path in AudioUnitPlugin.installed_plugins
+        if any(plugin_name in plugin_path.lower() for plugin_name in plugins_filter)
+    ]
+
+    print(  f"Plugin filters: {filters}")
+
+    print("  Filtered VST3 Plugins:")
+    pprint_with_indent(filtered_vst3_plugins, indent=4)
+
+    print("  Filtered AudioUnit Plugins:")
+    pprint_with_indent(filtered_au_plugins, indent=4)
+
+# Compare the plugin parameters between the VST3 and AudioUnit versions of a plugin, showing the differences and similarities
+#
+# Example usage:
+#   compare_plugin_parameters(
+#     "/Library/Audio/Plug-Ins/VST3/Vital.vst3",
+#     "/Library/Audio/Plug-Ins/Components/Vital.component"
+#   )
+def compare_plugin_parameters(vst3_path, au_path):
+    print("Comparing plugin parameters between VST3 and AudioUnit plugins:")
+    print("  Loading VST3 plugin..")
+    vst3_plugin = load_plugin(vst3_path)
+    print("  Successfully loaded VST3 plugin.")
+
+    print("  Loading AudioUnit plugin..")
+    au_plugin = load_plugin(au_path)
+    print("  Successfully loaded AudioUnit plugin.")
+
+    vst3_params = set(vst3_plugin.parameters.keys())
+    au_params = set(au_plugin.parameters.keys())
+
+    only_in_vst3 = vst3_params - au_params
+    only_in_au = au_params - vst3_params
+    in_both = vst3_params & au_params
+
+    print("  Parameters only in VST3:", only_in_vst3)
+    print("  Parameters only in AU:", only_in_au)
+    print("  Parameters in both:", in_both)
+
+# Example usage:
+# Assuming synth_plugin.parameters['verb_wet'] is your parameter:
+#   verb_wet = synth_plugin.parameters['verb_wet']
+#   print_parameter_properties(verb_wet)
+def print_parameter_properties(parameter):
+    """
+    Helper function to print out properties of an AudioProcessorParameter.
+
+    Args:
+    - parameter: An instance of AudioProcessorParameter.
+
+    See: https://github.com/spotify/pedalboard/blob/f2c2ccd64e78abaf9b87bc2c59097965c8b92fe5/pedalboard/ExternalPlugin.h#L1307-L1310
+    """
+    # List of property names to display
+    properties = [
+        'index',
+        'name',
+        'python_name',
+        'string_value',
+        'raw_value',
+        'default_raw_value',
+        'range',
+        'max_value',
+        'min_value',
+        'step_size',
+        'approximate_step_size',
+        'num_steps',
+        'type',
+        'units',
+        'label',
+        'is_discrete',
+        'is_boolean',
+        'is_orientation_inverted',
+        'is_automatable',
+        'is_meta_parameter',
+    ]
+
+    # Iterate over the property names
+    for property_name in properties:
+        try:
+            # Access the property value
+            property_value = getattr(parameter, property_name)
+            # Print the property name and its value
+            pprint(f"{property_name}: {property_value}")
+        except AttributeError as e:
+            # If the property does not exist, print an error message
+            pprint(f"{property_name}: Property does not exist. Error: {e}")
+
